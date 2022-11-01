515abb1a94c001c24887b4275dee539e11289f74
Duy Pham <duypham9895@gmail.com>
2022-11-01 15:46:06 +0700

    Hidden key db

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'patient-morning-585796.cloud.neon.tech'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'main'),
-      user: env('DATABASE_USERNAME', 'duypham9895'),
-      password: env('DATABASE_PASSWORD', 'oKpt2u1DgiTW'),
-      ssl: env.bool('DATABASE_SSL', true),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
