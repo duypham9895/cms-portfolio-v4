bf19941dc2801921b9f5bc1ce4e98eb96b087de9
Duy Pham <duypham9895@gmail.com>
2022-11-01 17:55:16 +0700

    Config folder to upload images to Cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,14 +1,16 @@
 export default ({ env }) => ({
   upload: {
     config: {
-      provider: 'cloudinary',
+      provider: "cloudinary",
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
       },
       actionOptions: {
-        upload: {},
+        upload: {
+          folder: env("CLOUDINARY_FOLDER"),
+        },
         delete: {},
       },
     },
